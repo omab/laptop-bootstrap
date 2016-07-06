274ad777dd50c9d6231847cbe913340e2fbc76db
Matías Aguirre <matiasaguirre@gmail.com>
2016-07-06 20:19:29 -0300

    Debian bootstrap

---
+++ debian/bootstrap.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -x
+
+unset UCF_FORCE_CONFFOLD
+export DEBIAN_FRONTEND=noninteractive
+export UCF_FORCE_CONFFNEW=YES
+
+ucf --purge /boot/grub/menu.lst
+sudo dpkg-reconfigure debconf -f noninteractive -p critical
+
+APT_GET="sudo apt-get"
+APT_GET_INSTALL="$APT_GET -qy --no-install-recommends install"
+
+# configure sudo
+# omab ALL=(ALL) NOPASSWD:ALL
+
+# update to testing
+# echo "deb http://mirrors.kernel.org/debian/ testing main contrib non-free" > /etc/apt/sources.list
+# $APT_GET update
+# $APT_GET upgrade
+# $APT_GET dist-upgrade
+
+# REBOOT to boot into the new kernel
+
+# update to sid
+grep -e sid /etc/debian_version || (
+    echo "deb http://mirrors.kernel.org/debian/ sid main contrib non-free" | sudo tee /etc/apt/sources.list
+    $APT_GET update
+    $APT_GET -qy upgrade
+    $APT_GET -qy dist-upgrade
+    # REBOOT to boot into the new kernel
+)
+
+# restore backup
+
+$APT_GET -qy autoremove gdm3
+$APT_GET -qy autoremove --purge 'gnome*'
+
+$APT_GET_INSTALL git curl awesome awesome-extra suckless-tools slim \
+                 scrot vim-gtk sakura ack-grep vagrant mpv unp \
+                 nfs-kernel-server nfs-common arandr evince geeqie \
+                 feh nodejs npm gparted htop aspell-en aspell-es \
+                 i3lock zfsutils-linux zfs-dkms unp redshift \
+                 weechat weechat-scripts weechat-plugins \
+                 virtualenv virtualenvwrapper autojump zsh \
+                 ruby pry ipython nodejs npm
+
+# Fetch dotfiles
+curl -s https://raw.githubusercontent.com/omab/dotfiles/master/.bin/dotfiles-bootstrap | bash -
+
+# Configure brightness
+[[ -d /usr/share/X11/xorg.conf.d ]] && (
+cat <<EOF | sudo tee /usr/share/X11/xorg.conf.d/20-intel.conf
+Section "Device"
+  Identifier  "card0"
+  Driver      "intel"
+  Option      "Backlight"  "intel_backlight"
+  BusID       "PCI:0:2:0"
+EndSection
+EOF
+)
+
+# install chrome / virtualbox
+
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget -q https://dl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
+
+echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+$APT_GET update
+$APT_GET_INSTALL --allow-unauthenticated google-chrome-stable virtualbox virtualbox-ext-pack
+sudo ln -sf /usr/bin/google-chrome /usr/local/bin/chrome
+
+# gems / pip / etc packages
+
+sudo gem install rubocop
+sudo npm install -g eslint babel-eslint eslint-plugin-react
+$APT_GET_INSTALL python-flake8 pylint pylint3
